afabca093122ce89a90211e13c4ea025defd5721
Onkarok1 <onkarok@rediffmail.com>
2019-03-08 16:11:50 +0530

    test

--- src/test/java/com/qaagility/controller/CalcTest.java
+++ src/test/java/com/qaagility/controller/CalcTest.java
@@ -2,7 +2,8 @@ package com.qaagility.controller;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
- 
+import com.qaagility.javaee.*;
+
 public class CalcTest {
 
       public void testSlow() {
@@ -26,6 +27,18 @@ public class CalcTest {
       public void testCalc1() {
         assertEquals("Result - Fail", 9, 9);
       }
+
+      	@Test
+      public void testCal() {
+
+        Calcmul testcalmul = new Calcmul();
+        Calculator testcaladd = new Calculator();
+
+        assertEquals(testcalmul.mul(),18);
+	assertEquals(testcaladd.add(),9);
+
+	}
+
 	
 	
     }
